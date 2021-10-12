0f63972078c903a535e85d1ebdbc1c0f834f2cde
fanghenshao <fanghenshao@sjtu.edu.cn>
2021-10-12 21:36:27 +0800

    refine attack

--- attack_dio.py
+++ attack_dio.py
@@ -25,6 +25,7 @@ from utils import setup_seed
 import numpy as np
 
 from autoattack import AutoAttack
+from attackers import fgsm_attack, pgd_attack, cw_attack
 
 # ======== fix data type ========
 torch.set_default_tensor_type(torch.FloatTensor)
@@ -157,7 +158,7 @@ def main():
             acc_fgsm = []
             for eps in fgsm_epsilons:
                 attack_param['eps'] = eps
-                corr_te_fgsm, _, _ = attack(backbone, head, idx, testloader, attack_param)
+                corr_te_fgsm = attack(backbone, head, idx, testloader, attack_param)
                 acc_fgsm.append(corr_te_fgsm/float(test_num))
 
             avg_acc_fgsm[idx,:] = avg_acc_fgsm[idx,:] + np.array(acc_fgsm)
@@ -180,7 +181,7 @@ def main():
             acc_pgd = []
             for eps in pgd_epsilons:
                 attack_param['eps'] = eps
-                corr_te_pgd, _, _ = attack(backbone, head, idx, testloader, attack_param)
+                corr_te_pgd = attack(backbone, head, idx, testloader, attack_param)
                 acc_pgd.append(corr_te_pgd/float(test_num))
 
             avg_acc_pgd[idx,:] = avg_acc_pgd[idx,:] + np.array(acc_pgd)
@@ -206,7 +207,7 @@ def main():
         acc_cw = []
         for idx in range(args.num_heads):
             print("-------- attacking network-%d..."%idx)
-            corr_te_cw, _, _ = attack(backbone, head, idx, testloader, attack_param)
+            corr_te_cw = attack(backbone, head, idx, testloader, attack_param)
             acc_te_cw = corr_te_cw / float(test_num)
             acc_cw.append(acc_te_cw)
 
@@ -229,8 +230,7 @@ def main():
 
         acc_aa = []
         for idx in range(args.num_heads):
-            print("-------- attacking network-%d..."%idx)
-            corr_te_aa, _, _ = attack(backbone, head, idx, testloader, attack_param)
+            corr_te_aa = attack(backbone, head, idx, testloader, attack_param)
             break
         acc_te_aa = corr_te_aa / float(test_num)
         acc_aa.append(acc_te_aa)
@@ -251,8 +251,7 @@ def main():
 
         acc_aa = []
         for idx in range(args.num_heads):
-            print("-------- attacking network-%d..."%idx)
-            corr_te_aa, _, _ = attack(backbone, head, idx, testloader, attack_param)
+            corr_te_aa = attack(backbone, head, idx, testloader, attack_param)
             break
         acc_te_aa = corr_te_aa / float(test_num)
         acc_aa.append(acc_te_aa)
@@ -273,8 +272,7 @@ def main():
 
         acc_aa = []
         for idx in range(args.num_heads):
-            print("-------- attacking network-%d..."%idx)
-            corr_te_aa, _, _ = attack(backbone, head, idx, testloader, attack_param)
+            corr_te_aa = attack(backbone, head, idx, testloader, attack_param)
             break
         acc_te_aa = corr_te_aa / float(test_num)
         acc_aa.append(acc_te_aa)
@@ -321,130 +319,6 @@ def evaluate(backbone, head, trainloader, testloader):
         
     return correct_train, correct_test
 
-# -------- FGSM attack --------
-def fgsm_attack(backbone, head, idx, image, label, epsilon):
-    image.requires_grad = True
-
-    # _, logits = net(image, idx)
-    logits = head(backbone(image), idx)
-    loss = F.cross_entropy(logits, label)
-    # net.zero_grad()
-    backbone.zero_grad()
-    head.zero_grad()
-    loss.backward()
-
-    # collect data grad
-    perturbed_image = image + epsilon*image.grad.data.sign()
-    # clip the perturbed image into [0,1]
-    perturbed_image = torch.clamp(perturbed_image, 0, 1)
-    return perturbed_image, None
-
-# -------- PGD attack --------
-def pgd_attack(backbone, head, idx, image, label, eps, alpha=0.01, iters=7, random_start=True, d_min=0, d_max=1):
-
-    perturbed_image = image.clone()
-    perturbed_image.requires_grad = True
-
-    image_max = image + eps
-    image_min = image - eps
-    image_max.clamp_(d_min, d_max)
-    image_min.clamp_(d_min, d_max)
-
-    if random_start:
-        with torch.no_grad():
-            perturbed_image.data = image + perturbed_image.uniform_(-1*eps, eps)
-            perturbed_image.data.clamp_(d_min, d_max)
-    
-    for index in range(iters):
-        # net.zero_grad()
-        backbone.zero_grad()
-        head.zero_grad()
-        # _, logits = net(perturbed_image, idx)
-        logits = head(backbone(perturbed_image), idx)
-
-        loss = F.cross_entropy(logits, label)
-        if perturbed_image.grad is not None:
-            perturbed_image.grad.data.zero_()
-        
-        loss.backward()
-        data_grad = perturbed_image.grad.data
-
-        with torch.no_grad():
-            perturbed_image.data += alpha * torch.sign(data_grad)
-            perturbed_image.data = torch.max(torch.min(perturbed_image, image_max), image_min)
-    perturbed_image.requires_grad = False
-    
-    return perturbed_image, None
-
-# -------- c&w attack --------
-def tanh_space(x):
-    return 1/2*(torch.tanh(x) + 1)
-
-def inverse_tanh_space(x):
-    # torch.atanh is only for torch >= 1.7.0
-    return atanh(x*2-1) 
-
-def atanh(x):
-    return 0.5*torch.log((1+x)/(1-x))
-
-def f(outputs, labels, kappa):
-    one_hot_labels = torch.eye(len(outputs[0]))[labels].cuda()
-
-    i, _ = torch.max((1-one_hot_labels)*outputs, dim=1)
-    j = torch.masked_select(outputs, one_hot_labels.bool())
-
-    return torch.clamp((-1)*(i-j), min=-kappa)
-
-def cw_attack(backbone, head, idx, image, label, c, kappa, n_iters, lr):
-    image = image.clone().detach().cuda()
-    label = label.clone().detach().cuda()
-
-    w = inverse_tanh_space(image).detach()
-    w.requires_grad = True
-
-    best_adv_images = image.clone().detach()
-    best_L2 = 1e10*torch.ones((len(image))).cuda()
-    prev_cost = 1e10
-    dim = len(image.shape)
-
-    MSELoss = nn.MSELoss(reduction='none')
-    Flatten = nn.Flatten()
-
-    optimizer = optim.Adam([w], lr=lr)
-
-    for step in range(n_iters):
-        adv_images = tanh_space(w)
-
-        current_L2 = MSELoss(Flatten(adv_images), Flatten(image)).sum(dim=1)
-        L2_loss = current_L2.sum()
-        
-        outputs = head(backbone(adv_images), idx)
-        f_loss = f(outputs, label, kappa).sum()
-
-        cost = L2_loss + c*f_loss
-
-        optimizer.zero_grad()
-        cost.backward()
-        optimizer.step()
-        
-        # Update Adversarial Images
-        _, pre = torch.max(outputs.detach(), 1)
-        correct = (pre == label).float()
-        
-        mask = (1-correct)*(best_L2 > current_L2.detach())
-        best_L2 = mask*current_L2.detach() + (1-mask)*best_L2
-        
-        mask = mask.view([-1]+[1]*(dim-1))
-        best_adv_images = mask*adv_images.detach() + (1-mask)*best_adv_images
-        
-        # Early Stop when loss does not converge.
-        if step % (n_iters//10) == 0:
-            if cost.item() > prev_cost:
-                return best_adv_images
-            prev_cost = cost.item()
-            
-    return best_adv_images
-
 # -------- attack model --------
 def attack(backbone, head, idx, testloader, attack_param):
 
@@ -478,9 +352,9 @@ def attack(backbone, head, idx, testloader, attack_param):
 
         # generate adversarial examples
         if args.attack_type == "fgsm":
-            perturbed_image, _ = fgsm_attack(backbone, head, idx, image, label, attack_param['eps'])
+            perturbed_image = fgsm_attack(backbone, head, idx, image, label, attack_param['eps'])
         elif args.attack_type == "pgd":
-            perturbed_image, _ = pgd_attack(backbone, head, idx, image, label, attack_param['eps'])
+            perturbed_image = pgd_attack(backbone, head, idx, image, label, attack_param['eps'])
         elif args.attack_type == "cw":
             c, kappa, n_iters, lr = attack_param['c'], attack_param['kappa'], attack_param['n_iters'], attack_param['lr']
             perturbed_image = cw_attack(backbone, head, idx, image, label, c=c, kappa=kappa, n_iters=n_iters, lr=lr)
@@ -501,7 +375,7 @@ def attack(backbone, head, idx, testloader, attack_param):
             _, pred = torch.max(logits.data, 1)
             correct += (pred==label).sum().item()
     
-    return correct, None, None
+    return correct
 
 
 # -------- start point
--- attackers.py
+++ attackers.py
@@ -1,34 +1,22 @@
 import torch
 import torch.nn.functional as F
 
-"""
-The PGD attack function in this file is used for adversarial training.
 
-During adversarial training of OMP model, at each iteration of PGD attack,
-    we RANDOMLY select a path to perform attack.
+# -------- FGSM attack --------
+def fgsm_attack(backbone, head, idx, image, label, epsilon):
+    image.requires_grad = True
 
-"""
+    logits = head(backbone(image), idx)
+    loss = F.cross_entropy(logits, label)
+    backbone.zero_grad()
+    head.zero_grad()
+    loss.backward()
 
-# # -------- FGSM attack --------
-# def fgsm_attack(net, image, label, epsilon):
-#     image.requires_grad = True
-
-#     _, logits = net(image, 'random')
-#     loss = F.cross_entropy(logits, label)
-
-#     # print("batch avg. loss = %f"%torch.mean(loss))
-
-#     net.zero_grad()
-#     loss.backward()
-
-#     # collect data grad sign sum
-#     batch_grad_sign_sum = torch.sum(torch.abs(image.grad.data.sign()))
-
-#     # collect data grad
-#     perturbed_image = image + epsilon*image.grad.data.sign()
-#     # clip the perturbed image into [0,1]
-#     perturbed_image = torch.clamp(perturbed_image, 0, 1)
-#     return perturbed_image, batch_grad_sign_sum
+    # collect data grad
+    perturbed_image = image + epsilon*image.grad.data.sign()
+    # clip the perturbed image into [0,1]
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    return perturbed_image
 
 # -------- PGD attack --------
 def pgd_attack(backbone, head, image, label, eps, alpha=0.01, iters=7, random_start=True, d_min=0, d_max=1):
@@ -41,19 +29,14 @@ def pgd_attack(backbone, head, image, label, eps, alpha=0.01, iters=7, random_st
     image_max.clamp_(d_min, d_max)
     image_min.clamp_(d_min, d_max)
 
-    # initialize average batch data gradient sign sum
-    avg_batch_grad_sign_sum = 0
-
     if random_start:
         with torch.no_grad():
             perturbed_image.data = image + perturbed_image.uniform_(-1*eps, eps)
             perturbed_image.data.clamp_(d_min, d_max)
     
     for idx in range(iters):
-        # net.zero_grad()
         backbone.zero_grad()
         head.zero_grad()
-        # _, logits = net(perturbed_image, 'random')
         logits = head(backbone(perturbed_image), 'random')
 
         loss = F.cross_entropy(logits, label)
@@ -63,16 +46,78 @@ def pgd_attack(backbone, head, image, label, eps, alpha=0.01, iters=7, random_st
         loss.backward()
         data_grad = perturbed_image.grad.data
 
-        # collect batch grad sign sum in each iteration
-        avg_batch_grad_sign_sum = avg_batch_grad_sign_sum + torch.sum(torch.abs(torch.sign(data_grad)))
-
         with torch.no_grad():
             perturbed_image.data += alpha * torch.sign(data_grad)
             perturbed_image.data = torch.max(torch.min(perturbed_image, image_max), image_min)
     perturbed_image.requires_grad = False
     
-    # compute average batch grad sign sum
-    avg_batch_grad_sign_sum = avg_batch_grad_sign_sum / iters
-    
-    return perturbed_image, avg_batch_grad_sign_sum
+    return perturbed_image
 
+# -------- c&w attack --------
+def tanh_space(x):
+    return 1/2*(torch.tanh(x) + 1)
+
+def inverse_tanh_space(x):
+    # torch.atanh is only for torch >= 1.7.0
+    return atanh(x*2-1) 
+
+def atanh(x):
+    return 0.5*torch.log((1+x)/(1-x))
+
+def f(outputs, labels, kappa):
+    one_hot_labels = torch.eye(len(outputs[0]))[labels].cuda()
+
+    i, _ = torch.max((1-one_hot_labels)*outputs, dim=1)
+    j = torch.masked_select(outputs, one_hot_labels.bool())
+
+    return torch.clamp((-1)*(i-j), min=-kappa)
+
+def cw_attack(backbone, head, idx, image, label, c, kappa, n_iters, lr):
+    image = image.clone().detach().cuda()
+    label = label.clone().detach().cuda()
+
+    w = inverse_tanh_space(image).detach()
+    w.requires_grad = True
+
+    best_adv_images = image.clone().detach()
+    best_L2 = 1e10*torch.ones((len(image))).cuda()
+    prev_cost = 1e10
+    dim = len(image.shape)
+
+    MSELoss = nn.MSELoss(reduction='none')
+    Flatten = nn.Flatten()
+
+    optimizer = optim.Adam([w], lr=lr)
+
+    for step in range(n_iters):
+        adv_images = tanh_space(w)
+
+        current_L2 = MSELoss(Flatten(adv_images), Flatten(image)).sum(dim=1)
+        L2_loss = current_L2.sum()
+        
+        outputs = head(backbone(adv_images), idx)
+        f_loss = f(outputs, label, kappa).sum()
+
+        cost = L2_loss + c*f_loss
+
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+        
+        # Update Adversarial Images
+        _, pre = torch.max(outputs.detach(), 1)
+        correct = (pre == label).float()
+        
+        mask = (1-correct)*(best_L2 > current_L2.detach())
+        best_L2 = mask*current_L2.detach() + (1-mask)*best_L2
+        
+        mask = mask.view([-1]+[1]*(dim-1))
+        best_adv_images = mask*adv_images.detach() + (1-mask)*best_adv_images
+        
+        # Early Stop when loss does not converge.
+        if step % (n_iters//10) == 0:
+            if cost.item() > prev_cost:
+                return best_adv_images
+            prev_cost = cost.item()
+            
+    return best_adv_images
