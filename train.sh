c1700456e6272eb0cd0584b276acd64e6f20f37e
fanghenshaometeor <fanghenshao@sjtu.edu.cn>
2022-06-12 09:35:17 -0400

    update-training-scripts

--- train.sh
+++ train.sh
@@ -1,32 +1,15 @@
-# -------- model:vgg16/resnet18 -----------------
-# arch=wrn34x10
-arch=preactresnet18
-# -------- hyper-parameters ---------------------
-alpha=0.1
-beta=0.1
-tau=1.0
-# ----
-num_heads=10
-# -------- CIFAR10 ------------------------------
-dataset=CIFAR10
-data_dir='~/KunFang/data/CIFAR10/'
-# -------- CIFAR100 -----------------------------
-# dataset=CIFAR100
-# data_dir='~/KunFang/data/CIFAR100/'
-# -------- SVNH ---------------------------------
-# dataset=SVHN
-# data_dir='~/KunFang/data/SVHN/'
-# -----------------------------------------------
-# adv_train=False
-adv_train=True
-# -----------------------------------------------
-
-CUDA_VISIBLE_DEVICES=1 python train.py \
-    --arch ${arch} \
-    --num_heads ${num_heads} \
-    --alpha ${alpha} \
-    --beta ${beta} \
-    --tau ${tau} \
-    --dataset ${dataset} \
-    --data_dir ${data_dir} \
-    --adv_train ${adv_train}
+# -------- Vanilla-training DIO: CIFAR10/CIFAR100-PRN18
+# ---- CIFAR10-PRN18
+CUDA_VISIBLE_DEVICES=2 python train.py --arch preactresnet18 --dataset CIFAR10 --data_dir '~/KunFang/data/CIFAR10/' --adv_train False \
+                                    --alpha 0.1 --beta 0.1 --tau 0.2 --num_heads 10
+# ---- CIFAR100-PRN18
+CUDA_VISIBLE_DEVICES=2 python train.py --arch preactresnet18 --dataset CIFAR100 --data_dir '~/KunFang/data/CIFAR100/' --adv_train False \
+                                    --alpha 0.1 --beta 0.1 --tau 0.01 --num_heads 40
+                                
+# -------- Adversarial-training DIO (AT+DIO): CIFAR10-PRN18/WRN34X10
+# ---- CIFAR10-PRN18
+CUDA_VISIBLE_DEVICES=2 python train.py --arch preactresnet18 --dataset CIFAR10 --data_dir '~/KunFang/data/CIFAR10/' --adv_train True \
+                                    --alpha 0.1 --beta 0.1 --tau 0.2 --num_heads 10
+# ---- CIFAR10-WRN34X10
+CUDA_VISIBLE_DEVICES=2 python train.py --arch wrn34x10 --dataset CIFAR10 --data_dir '~/KunFang/data/CIFAR10/' --adv_train True \
+                                    --alpha 0.1 --beta 0.1 --tau 0.5 --num_heads 10
