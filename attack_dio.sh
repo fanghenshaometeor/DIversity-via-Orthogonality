10c1bf6cba2099ecea22e257340ee5d8737cdc21
fanghenshaometeor <fanghenshao@sjtu.edu.cn>
2022-06-12 11:54:41 -0400

    update-typo

--- attack_dio.sh
+++ attack_dio.sh
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=2 python attack_dio.py --arch wrn34x10 --num_heads 10 --dat
                     --attack_type 'None' #['None', 'pgd, 'pgd100', 'square', 'aa']
 # ---- CIFAR10-WRN34X10 best model
 CUDA_VISIBLE_DEVICES=2 python attack_dio.py --arch wrn34x10 --num_heads 10 --dataset CIFAR10 --data_dir '~/KunFang/data/CIFAR10/' \
-                    --model_path './save/CIFAR10/wrn34x10-adv/p-10-a-0.1-b-0.1-tau-0.5/best.pth' \        # last model
+                    --model_path './save/CIFAR10/wrn34x10-adv/p-10-a-0.1-b-0.1-tau-0.5/best.pth' \        # best model
                     --attack_type 'None' #['None', 'pgd, 'pgd100', 'square', 'aa']
 
 
